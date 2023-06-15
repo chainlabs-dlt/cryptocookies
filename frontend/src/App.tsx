58c69d2bb1d41d2621c1ef897c5c18292371d403
Alexis Schlomer <aschlome@andrew.cmu.edu>
2023-06-15 15:58:51 +0200

    Reintegrate usedapp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,47 @@ import { OrbitControls } from "@react-three/drei";
 import "./App.css";
 import Loading from "./components/Loading";
 
+import { Contract, BigNumber, utils } from 'ethers'
+import CKI from "./abis/Cki.json";
+import { useEthers, useEtherBalance, useCall, useContractFunction } from '@usedapp/core'
+import { formatEther } from '@ethersproject/units'
+import { Chainlabs } from ".";
+import StackingMenu from "./components/StackingMenu";
+
+function Bal(tokenAddress: string, account: string | undefined): BigNumber | undefined {
+	const { value, error } = useCall(account && {
+	  contract: new Contract(tokenAddress, new utils.Interface(CKI)),
+	  method: 'balanceOf',
+	  args: [account]
+	}) ?? {}
+	if(error) {
+	  console.error(error.message)
+	  return undefined
+	}
+	return value?.[0]
+ }
+
+function Mint(tokenAddress: string) {
+	const contract = new Contract(tokenAddress, new utils.Interface(CKI))
+
+	const { send } = useContractFunction(contract, 'devMint', {
+		transactionName: 'Mint',
+		gasLimitBufferPercentage: 10,
+	})
+
+	return (amount: BigNumber) => {
+		void send(amount);
+	}
+}
+
 function App() {
+	const { activateBrowserWallet, account, deactivate } = useEthers();
+	const chainlabsBalance = useEtherBalance(account, { chainId: Chainlabs.chainId });
+	const ckiBalance = Bal("0x5D6373b77c14ABf3FbBFe418DA4b4F0125c637FF", account);
+	const fdgBalance = Bal("0xE89A84Fd29eb0C35cEB7B1e13E567844Ed4DB361", account);
+	const ckiMint = Mint("0x5D6373b77c14ABf3FbBFe418DA4b4F0125c637FF");
+	const fdgMint = Mint("0xE89A84Fd29eb0C35cEB7B1e13E567844Ed4DB361");
+
 	return (
 		<div className="App">
 			<Loading />
@@ -61,9 +101,14 @@ function App() {
 
 			<div className="GUIAccountHeader">
 				<AccountHeader
-					onClick={function (): void {}}
-					accountName={"Dimitri"}
-					infos={"#1054 Suisse"}
+					onClick={function (): void {
+						if (!account) activateBrowserWallet();
+						else deactivate();
+					}}
+					accountName={account ? account.substring(0, 7) + "..." + account.substring(37, 42) : "Unconnected"}
+					infos={account ?
+						(chainlabsBalance ? Chainlabs.nativeCurrency?.symbol + " " + formatEther(chainlabsBalance) : "")
+						: "Click to connect"}
 				/>
 				<AssetsHeader
 					defensePercentage={50}
@@ -72,14 +117,24 @@ function App() {
 					attackPoints={10}
 					onClick={function (): void {}}
 				/>
-				<TokenHeader
-					onClick={function (): void {}}
-					stackingPercent={10}
-					lockingPercent={30}
-					token={TokenType.COOKIE}
-					amount={32350}
-					output={2}
-				/>
+				<div className="GUIGameTokens">
+					<TokenHeader
+						onClick={function (): void { fdgMint(BigNumber.from(10).pow(17).mul(4)) }}
+						stackingPercent={10}
+						lockingPercent={30}
+						token={TokenType.FUDGE}
+						amount={fdgBalance ? fdgBalance.div(BigNumber.from(10).pow(13)).toNumber() / 10**5 : 0}
+						output={2}
+					/>
+					<TokenHeader
+						onClick={function (): void { ckiMint(BigNumber.from(10).pow(17).mul(4)) }}
+						stackingPercent={40}
+						lockingPercent={30}
+						token={TokenType.COOKIE}
+						amount={ckiBalance ? ckiBalance.div(BigNumber.from(10).pow(13)).toNumber() / 10**5 : 0}
+						output={2}
+					/>
+				</div>
 			</div>
 			<div className="GUIBoostingFooter">
 				<WorldButton onClick={function (): void {}} />
