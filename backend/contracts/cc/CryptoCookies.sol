cc73be0805711fd81e81dbcff1c8ff11ec076583
Alexis Schlomer <aschlome@andrew.cmu.edu>
2023-07-04 18:15:20 +0200

    Continue file reorganization

---
+++ backend/contracts/cc/CryptoCookies.sol
@@ -0,0 +1,18 @@
+/// SPDX-License-Identifier: UNLICENSED
+/// Copyright © 2023 Chainlabs Switzerland SA
+/// All Rights Reserved
+
+
+pragma solidity ^0.8.18;
+
+/// @title CryptoCookies
+/// @author Chainlabs Switzerland SA
+/// @notice 
+contract CryptoCookies { // Upgradable? isDAO (heritance)
+    // Reference to Core (upgradable?)
+    // Reference to game DAO or is DAO directly (upgradable?)
+	// Reference to comptition or directly (upgradable?)
+
+    // Automatic setup, creates all pools for easy bootstrap
+	constructor() {}
+}
