4c9b5f373afcb15720b9145a98e940cd59a0129d
Emilien <emilien.duc@epfl.ch>
2023-07-10 14:10:24 +0200

    first draft claimable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import AccountHeader from "./components/AccountHeader";
 import AssetsHeader from "./components/AssetsHeader";
 import TokenHeader from "./components/TokenHeader";
@@ -10,48 +10,143 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import "./App.css";
 import Loading from "./components/Loading";
+import Popup from "reactjs-popup";
+import AssetsRepartitionPopup from "./components/AssetsRepartitionPopup";
 
 import { Contract, BigNumber, utils } from 'ethers'
-import CKI from "./abis/Cki.json";
-import { useEthers, useEtherBalance, useCall, useContractFunction } from '@usedapp/core'
+import { Bal, Mint, Stake, Claim, Approve, UserState, GlobalState, GetCkiDistr, GetFdgDistr} from './utils/Contracts';
+import { useEthers, useEtherBalance, useCall, useContractFunction, useBlockMeta } from '@usedapp/core'
 import { formatEther } from '@ethersproject/units'
 import { Chainlabs } from ".";
 import StackingMenu from "./components/StackingMenu";
+import { Decimal } from "decimal.js";
 
-function Bal(tokenAddress: string, account: string | undefined): BigNumber | undefined {
-	const { value, error } = useCall(account && {
-	  contract: new Contract(tokenAddress, new utils.Interface(CKI)),
-	  method: 'balanceOf',
-	  args: [account]
-	}) ?? {}
-	if(error) {
-	  console.error(error.message)
-	  return undefined
+
+
+function claimable(globalStateIndex: Decimal, userState: any) {
+	const userAccumulated: Decimal =  new Decimal(userState.ownAccumulatedTotal.toString());
+	var updatedFreshOwn: Decimal = new Decimal(0);
+
+	if (!BigNumber.from('0').eq(userState.ownStake)) {
+		console.log(userState.ownStake.toString());
+		console.log(userState.lastGlobalState.index.toString());
+		
+		updatedFreshOwn = 
+			(globalStateIndex.sub(new Decimal(userState.lastGlobalState.index.toString()))).mul(
+			new Decimal(userState.ownStake.toString())).div(
+			new Decimal(userState.lastGlobalState.incrementPerRevenue.toString()).mul(new Decimal(userState.lastGlobalState.totalStake.toString())));
 	}
-	return value?.[0]
- }
 
-function Mint(tokenAddress: string) {
-	const contract = new Contract(tokenAddress, new utils.Interface(CKI))
 
-	const { send } = useContractFunction(contract, 'devMint', {
-		transactionName: 'Mint',
-		gasLimitBufferPercentage: 10,
-	})
+	const accumulated = userAccumulated.plus(updatedFreshOwn);
 
-	return (amount: BigNumber) => {
-		void send(amount);
+	console.log(accumulated.toString());
+	const alreadyClaimed: Decimal = new Decimal(userState.alreadyClaimedTotal.toString());  
+	console.log(alreadyClaimed.toString());
+	return accumulated.sub(alreadyClaimed);
+}
+
+function updateGlobalIndex(index: Decimal, incrementPerRevenue: Decimal, revenue: Decimal) {
+	return index.add(incrementPerRevenue.mul(revenue));
+}
+
+function claimableCKI(userState: any, globalState: any, blockTimestamp: any, invExpDistr: any, emGamma: any) {
+	let globalStateIndex: Decimal = new Decimal(globalState.index.toString());
+
+	if (!new Decimal(invExpDistr.lastTrigger).equals(new Decimal(blockTimestamp))) {
+		const elapsed: Decimal = new Decimal(blockTimestamp) ;
+
+		const exp: Decimal = elapsed.div(new Decimal(emGamma.toString())).neg().exp();
+		const c: Decimal = new Decimal(invExpDistr.c.toString());
+		const rev: Decimal = c.sub(c.mul(exp)).abs();
+
+		const amount: Decimal = (rev.sub(new Decimal(invExpDistr.lastDistr.toString()))).mul(new Decimal(10 ** 18));
+
+		if (!amount.isZero()) {
+			globalStateIndex = updateGlobalIndex(globalStateIndex, new Decimal(globalState.incrementPerRevenue.toString()), amount);
+		}
 	}
+
+	return claimable(globalStateIndex, userState);
+}
+
+function claimableFDG(userState: any, globalState: any, blockTimestamp: any, flatDistr: any, flattenPeriod: any) {
+	const elapsed: Decimal = new Decimal(blockTimestamp).sub(new Decimal(flatDistr.lastDistr.toString()));
+	const distrBuffer: Decimal  = new Decimal(flatDistr.distrBuffer.toString());
+	let globalStateIndex = new Decimal(globalState.index.toString());
+	const incrementPerRevenue = new Decimal(globalState.incrementPerRevenue.toString());
+	const flattenPeriodLength: Decimal = new Decimal(flattenPeriod.toString());
+
+	if(elapsed.isZero() || distrBuffer.isZero()) {
+		return claimable(globalStateIndex, userState);
+	}
+
+	if(elapsed.greaterThanOrEqualTo(flattenPeriodLength)) {
+		globalStateIndex = updateGlobalIndex(globalStateIndex, incrementPerRevenue, distrBuffer);
+		// TODO, what if is has been more than 1 period??
+	} else {
+		const revenue = (elapsed.mul(distrBuffer)).div(flattenPeriodLength);
+		globalStateIndex = updateGlobalIndex(globalStateIndex, incrementPerRevenue, revenue);
+	}
+
+	return claimable(globalStateIndex, userState);
 }
 
 function App() {
+
 	const { activateBrowserWallet, account, deactivate } = useEthers();
+
+	const date: Date | undefined = useBlockMeta().timestamp;
+	let timestamp: any;
+	if (date) {
+		timestamp = Math.floor(date.getTime() / 1000);
+	} else {
+		console.log('Error: timestamp is undefined')
+		timestamp = 0
+	}
+
+
 	const chainlabsBalance = useEtherBalance(account, { chainId: Chainlabs.chainId });
 	const ckiBalance = Bal("0x5D6373b77c14ABf3FbBFe418DA4b4F0125c637FF", account);
 	const fdgBalance = Bal("0xE89A84Fd29eb0C35cEB7B1e13E567844Ed4DB361", account);
+
+	const ckiUserState = UserState("0x0a58c62697958311c82F6CA5645fb72aeBCD8522", account);
+	const ckiGlobalState = GlobalState("0x0a58c62697958311c82F6CA5645fb72aeBCD8522");
+
+	const fdgUserState = UserState("0x38122594740D9BFfde1a577Fb0692a52bF0d5F40", account);
+	const fdgGlobalState = GlobalState("0x38122594740D9BFfde1a577Fb0692a52bF0d5F40");
+
+	const invExpDistr = GetCkiDistr("0x49365d44f58Ba6351D731Ed170B18248B6aD77D3", 'invExpDistr');
+	const emGamma = GetCkiDistr("0x49365d44f58Ba6351D731Ed170B18248B6aD77D3", 'EM_GAMMA');
+
+	const flatDistr = GetFdgDistr("0xf6BA809452aD34B5088e60477322eC8546168061", 'flatDistr');
+	const flattenPeriodLength = GetFdgDistr("0xf6BA809452aD34B5088e60477322eC8546168061", 'FLATTEN_PERIOD');
+
+
 	const ckiMint = Mint("0x5D6373b77c14ABf3FbBFe418DA4b4F0125c637FF");
 	const fdgMint = Mint("0xE89A84Fd29eb0C35cEB7B1e13E567844Ed4DB361");
 
+	const stakeCKI = Stake("0x0a58c62697958311c82F6CA5645fb72aeBCD8522");
+	const approveCKI = Approve("0x5D6373b77c14ABf3FbBFe418DA4b4F0125c637FF");
+
+	const stakeFDG = Stake("0x38122594740D9BFfde1a577Fb0692a52bF0d5F40");
+	const approveFDG = Approve("0xE89A84Fd29eb0C35cEB7B1e13E567844Ed4DB361");
+
+	const claimCKI = Claim("0x49365d44f58Ba6351D731Ed170B18248B6aD77D3", account);
+	const claimFDG = Claim("0xf6BA809452aD34B5088e60477322eC8546168061", account);
+
+	const [openStaking, setOpenStaking] = useState<boolean>(false);
+	const closeStaking = () => setOpenStaking(false);
+	const [openRepartition, setOpenRepartition] = useState<boolean>(false);
+	const closeRepartition = () => setOpenRepartition(false);
+	const [isLocking, setIsLocking] = useState<boolean>(false);
+	const [repartitionToken, setRepartitionToken] = useState<TokenType>(TokenType.COOKIE);
+
+	const openRepartitionPopup = (isLocking: boolean, tokentype: TokenType) => {
+		setIsLocking(isLocking);
+		setRepartitionToken(tokentype);
+		setOpenRepartition((o: boolean): boolean => !o);
+	}
 	return (
 		<div className="App">
 			<Loading />
@@ -66,16 +161,26 @@ function App() {
 				>
 					<Scene
 						onMountainClick={function (): void {
-							console.log("mountain");
+							console.log('FDG Balance = ' + fdgBalance?.toString());
+							console.log('CKI Balance = ' + ckiBalance?.toString());
+							console.log('Claimable CKI = ' + claimableCKI(fdgUserState, fdgGlobalState, timestamp, invExpDistr, emGamma).div(new Decimal(10**18)).toString());
+							console.log('Claimable FDG = ' + claimableFDG(ckiUserState, ckiGlobalState, timestamp, flatDistr, flattenPeriodLength).div(new Decimal(10**18)).toString());
 						}}
 						onCottageClick={function (): void {
 							console.log("cottage");
+							setOpenStaking((o: boolean): boolean => !o)
 						}}
-						onMineEntranceClick={function (): void {
-							console.log("mine entrance");
-						}}
+						onMineEntranceClick={
+							function (): void { 
+								// approveFDG("0x38122594740D9BFfde1a577Fb0692a52bF0d5F40", BigNumber.from(10).pow(17));
+								stakeFDG(BigNumber.from(10).pow(17));
+							 }
+						}
 						onPumpClick={function (): void {
 							console.log("pump");
+							// claimCKI();
+							claimFDG();
+							// openRepartitionPopup(true, TokenType.COOKIE);
 						}}
 						onAnvilClick={function (): void {
 							console.log("anvil");
@@ -97,7 +202,36 @@ function App() {
 				</Canvas>
 			</Suspense>
 
-			{/* Start of the GUI Part, to complete with what you wish and your business logic*/}
+			{/* Start of the GUI Part */}
+
+
+			<Popup open={openStaking} closeOnDocumentClick onClose={closeStaking}>
+				<StackingMenu
+					onClose={() => { }}
+					onCookieSelected={() => {
+						closeStaking();
+						openRepartitionPopup(false, TokenType.COOKIE);
+					}}
+					onFudgeSelected={() => {
+						closeStaking();
+						openRepartitionPopup(false, TokenType.FUDGE);
+					}}
+				/>
+			</Popup>
+
+			<Popup open={openRepartition} closeOnDocumentClick onClose={closeRepartition}>
+				<AssetsRepartitionPopup
+					amount={130}
+					isLocking={isLocking}
+					lockingPercent={40}
+					onChangeAmount={() => { }}
+					onClose={() => { }}
+					stackingPercent={20}
+					token={repartitionToken}
+				/>
+			</Popup>
+
+
 
 			<div className="GUIAccountHeader">
 				<AccountHeader
@@ -119,23 +253,24 @@ function App() {
 				/>
 				<div className="GUIGameTokens">
 					<TokenHeader
-						onClick={function (): void { fdgMint(BigNumber.from(10).pow(17).mul(4)) }}
+						onClick={function (): void { ckiMint(BigNumber.from(10).pow(17).mul(4)) }}
 						stackingPercent={10}
 						lockingPercent={30}
 						token={TokenType.FUDGE}
 						amount={fdgBalance ? fdgBalance.div(BigNumber.from(10).pow(13)).toNumber() / 10**5 : 0}
 						output={2}
 					/>
-					<TokenHeader
+					{/* <TokenHeader
 						onClick={function (): void { ckiMint(BigNumber.from(10).pow(17).mul(4)) }}
 						stackingPercent={40}
 						lockingPercent={30}
 						token={TokenType.COOKIE}
 						amount={ckiBalance ? ckiBalance.div(BigNumber.from(10).pow(13)).toNumber() / 10**5 : 0}
-						output={2}
-					/>
+						output={ckiUserState ? ckiUserState.ownStake.div(BigNumber.from(10).pow(13)).toNumber() / 10**5 : 0}
+					/> */}
 				</div>
 			</div>
+
 			<div className="GUIBoostingFooter">
 				<WorldButton onClick={function (): void {}} />
 				<BoostingFooter value={15} maxValue={20} width={300} height={20} />
---
+++ frontend/src/utils/Contracts.ts
@@ -0,0 +1,142 @@
+import { Contract, BigNumber, utils } from 'ethers'
+import CKI from "../abis/Cki.json";
+import BaseERC20Distr from "../abis/BaseERC20Distr.json";
+import CkiDistr from "../abis/CkiDistr.json";
+import CCStaking from "../abis/CCStaking.json";
+import FdgDistr from "../abis/FdgDistr.json";
+import { useEthers, useEtherBalance, useCall, useContractFunction, useBlockMeta } from '@usedapp/core'
+
+
+
+
+/*******************************
+ ** ERC-20 Related functions ***
+ ******************************/
+
+ export function Bal(tokenAddress: string, account: string | undefined): BigNumber | undefined {
+	const { value, error } = useCall(account && {
+	  contract: new Contract(tokenAddress, new utils.Interface(CKI)),
+	  method: 'balanceOf',
+	  args: [account]
+	}) ?? {}
+	if(error) {
+	  console.error(error.message)
+	  return undefined
+	}
+	return value?.[0]
+ }
+
+ export function Mint(tokenAddress: string) {
+	const contract = new Contract(tokenAddress, new utils.Interface(CKI))
+
+	const { send } = useContractFunction(contract, 'devMint', {
+		transactionName: 'Mint',
+		gasLimitBufferPercentage: 10,
+	})
+
+	return (amount: BigNumber) => {
+		void send(amount);
+	}
+}
+
+export function Approve(tokenAddress: string) {
+	const contract = new Contract(tokenAddress, new utils.Interface(CKI))
+
+	const { send } = useContractFunction(contract, 'approve', {
+		transactionName: 'approve',
+		gasLimitBufferPercentage: 10,
+	})
+
+	return (spender: string, amount: BigNumber) => {
+		void send(spender, amount);
+	}
+}
+
+
+/***************************************
+ *** Revenue Distribution  functions ***
+ **************************************/
+
+ export function Claim(tokenAddress: string, account: string | undefined) {
+	const contract = new Contract(tokenAddress, new utils.Interface(BaseERC20Distr))
+
+	const { send } = useContractFunction(contract, 'claim', {
+		transactionName: 'claim',
+		gasLimitBufferPercentage: 10,
+	})
+
+	return () => {
+		void send(account);
+	}
+}
+
+export function UserState(distrAddress: string, account: string | undefined) {
+	const { value, error } = useCall(account && {
+	  contract: new Contract(distrAddress, new utils.Interface(BaseERC20Distr)),
+	  method: 'usersState',
+	  args: [account]
+	}) ?? {}
+	if(error) {
+	  console.error(error.message)
+	  return undefined
+	}
+	return value;
+}
+
+export function GlobalState(distrAddress: string) {
+	const { value, error } = useCall({
+	  contract: new Contract(distrAddress, new utils.Interface(BaseERC20Distr)),
+	  method: 'globalState',
+	  args: []
+	}) ?? {}
+	if(error) {
+	  console.error(error.message)
+	  return undefined
+	}
+	return value;
+}
+
+/*********************************
+ ** Staking-specific functions ***
+ ********************************/
+
+ export function Stake(distrAddress: string) {
+	const contract = new Contract(distrAddress, new utils.Interface(CCStaking));
+	const { send } = useContractFunction(contract, 'stake', {
+		transactionName: 'stake',
+		gasLimitBufferPercentage: 10,
+	})
+
+	return (amount: BigNumber) => {
+		void send(amount);
+	}
+}
+
+
+export function GetCkiDistr(distrAddress: string, method: string) {
+	const { value, error } = useCall({
+	  contract: new Contract(distrAddress, new utils.Interface(CkiDistr)),
+	  method: method,
+	  args: []
+	}) ?? {}
+	if(error) {
+	  console.error(error.message)
+	  return undefined
+	}
+	return value;
+}
+
+export function GetFdgDistr(distrAddress: string, method: string) {
+    const { value, error } = useCall({
+        contract: new Contract(distrAddress, new utils.Interface(FdgDistr)),
+        method: method,
+        args: []
+      }) ?? {}
+      if(error) {
+        console.error(error.message)
+        return undefined
+      }
+      return value;
+}
+
+
